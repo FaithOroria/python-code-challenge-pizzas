4761b471417d790a469b822eeb69511c1f51132f
Faith Achieng <faith.achieng@student.moringaschool.com>
2023-11-29 19:47:31 +0300

    Completed

--- app/models.py
+++ app/models.py
@@ -1,10 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
 
 class Restaurant(db.Model):
-    __tablename__ = 'Restaurant'
-
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    address = db.Column(db.String(200))
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant')
 
-# add any models you may need. 
+class Pizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    ingredients = db.Column(db.String(200))
+    pizza_restaurants = db.relationship('RestaurantPizza', backref='pizza')
+
+class RestaurantPizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float)
+
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'))
+
+    @validates('price')
+    def validate_price(self, key, value):
+        if not 1 <= value <= 30:
+            raise ValueError("Price must be between 1 and 30")
+        return value
